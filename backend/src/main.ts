71511dd95bf19a3e6a71ebd3bd4944217c2edd8c
Avi <avi19042004@gmail.com>
2026-04-06 22:13:13 +0530

    Logger Setup Is Done

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,51 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+import 'winston-daily-rotate-file';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{
+    logger: WinstonModule.createLogger({
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.colorize(),
+            winston.format.printf(({ timestamp, level, message, context }) => {
+              return `[${timestamp}] ${level}: [${context || "App"}] ${message}`
+            })
+          )
+        }),
+
+        new winston.transports.DailyRotateFile({
+          filename: 'logs/errors-%DATE%.log',
+          datePattern: 'DD-MM-YYYY',
+          level: 'error',
+          zippedArchive: true,
+          maxSize: '20m',
+          maxFiles: '14d',
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.json(),
+          ),
+        }),
+
+        new winston.transports.DailyRotateFile({
+          filename: 'logs/logs-%DATE%.log',
+          datePattern: 'DD-MM-YYYY',
+          zippedArchive: true,
+          maxSize: '20m',
+          maxFiles: '30d',
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.json(),
+          ),
+        }),
+      ]
+    })
+  });
 
   app.use(helmet());
   app.enableCors({
